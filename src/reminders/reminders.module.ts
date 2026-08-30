c53ebbb1d9cd4adefdaafb15361de7454f735ea0
fekinadia <naddoutta25@gmail.com>
2026-08-30 12:38:28 +0100

    Add RemindersSchedulerService to RemindersModule

--- src/reminders/reminders.module.ts
+++ src/reminders/reminders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RemindersController } from './reminders.controller';
 import { RemindersService } from './reminders.service';
+import { RemindersSchedulerService } from './reminders-scheduler.service';
+import { SmsModule } from '../sms/sms.module';
 
 @Module({
+  imports: [SmsModule],
   controllers: [RemindersController],
-  providers: [RemindersService],
+  providers: [RemindersService, RemindersSchedulerService],
 })
 export class RemindersModule {}
