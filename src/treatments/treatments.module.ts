c9bf381a248f4fa39042820afecf46e315f843b2
fekinadia <naddoutta25@gmail.com>
2026-09-11 22:16:45 +0100

    Import AuditLogModule in TreatmentsModule

--- src/treatments/treatments.module.ts
+++ src/treatments/treatments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuditLogModule } from '../audit/audit-log.module';
 import { TreatmentsController } from './treatments.controller';
 import { TreatmentsService } from './treatments.service';
 
 @Module({
+  imports: [AuditLogModule],
   controllers: [TreatmentsController],
   providers: [TreatmentsService],
 })
